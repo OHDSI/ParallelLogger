debdfad62062eee6f53773878774830f776812cd
schuemie <schuemie@ohdsi.org>
2019-01-17 17:25:20 +0100

    Adding unit test

--- tests/testthat/test-saveAndLoadJson.R
+++ tests/testthat/test-saveAndLoadJson.R
@@ -12,3 +12,15 @@ test_that("data.frame restore", {
   expect_equal(class(settings$b), class(settings2$b))
   expect_equivalent(settings, settings2)
 })
+
+
+test_that("list with empty vectors", {
+  fileName = tempfile()
+  settings <- list(x = "a",
+                   y = c())
+  saveSettingsToJson(settings, fileName)
+  settings2 <- loadSettingsFromJson(fileName)
+  unlink(fileName)
+  expect_equal(class(settings$y), class(settings2$y))
+  expect_equivalent(settings, settings2)
+})
