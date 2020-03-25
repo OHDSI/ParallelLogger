c2d35ae8b5f9430a4656bacb87f0b123596e7ec7
Schuemie <MSCHUEMI@its.jnj.com>
2020-03-25 10:31:19 +0100

    Using roxygen "_PACKAGE" syntax to prevent new R check warning.

--- R/ParallelLogger.R
+++ R/ParallelLogger.R
@@ -16,10 +16,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-#' ParallelLogger
-#'
-#' @docType package
-#' @name ParallelLogger
+#' @keywords internal
+"_PACKAGE"
+
 #' @importFrom methods getFunction is
 #' @importFrom utils installed.packages install.packages menu capture.output help memory.limit setTxtProgressBar txtProgressBar
 NULL
