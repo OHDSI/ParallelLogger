ab95771c91a7cbfaa30cbe9bb1d82a8943380df7
Schuemie <MSCHUEMI@its.jnj.com>
2021-07-14 10:06:40 -0400

    Adding helpful comment

--- tests/testthat/test-args.R
+++ tests/testthat/test-args.R
@@ -4,4 +4,5 @@ test_that("Creation of args functions", {
   argsFun <- createArgFunction("createLogger")
   expect_true(any(grepl("#' @param", argsFun)))
   expect_true(any(grepl("createCreateLoggerArgs <- function", argsFun)))
+  #Hello
 })
