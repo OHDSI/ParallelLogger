1657b3decd67d69a05993c4c8227735830c3b674
icarusmaximus <76474833+icarusmaximus@users.noreply.github.com>
2021-07-15 16:14:22 +0200

    renamed appenders test file

--- tests/testthat/test-Appenders-email.R
+++
@@ -1,7 +0,0 @@
-#test nonsense email address
-test_that("testsendmail correct", {
-  to <- "bla"
-  subject <- "testing this mail"
-  body <- "Dear sir madam, I hope this test finds you well. Sincerely, John Doe"
-  expect_error(testSendMail(to, subject, body))
-})
---
+++ tests/testthat/test-Appenders.R
@@ -0,0 +1,15 @@
+#test nonsense email address
+test_that("testsendmail correct", {
+  to <- "bla"
+  subject <- "testing this mail"
+  body <- "Dear sir madam, I hope this test finds you well. Sincerely, John Doe"
+  expect_error(testSendMail(to, subject, body))
+})
+
+#test if output is produced
+test_that("testsendmail correct", {
+  to <- "johndoe@gmail.com"
+  subject <- "testing this mail"
+  body <- "Dear sir madam, I hop this test finds you well. Sincerely, John Doe"
+  expect_output(testSendMail(to, subject, body))
+})
