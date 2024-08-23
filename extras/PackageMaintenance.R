be3ac7213b8ec1261a43f441d141605434d59994
schuemie <schuemie@ohdsi.org>
2024-08-23 15:41:21 +0200

    Updating CRAN-SUBMISSION

--- extras/PackageMaintenance.R
+++ extras/PackageMaintenance.R
@@ -51,6 +51,6 @@ OhdsiRTools::fixHadesLogo()
 # Release package -----------------------------------------------------
 devtools::check_win_devel()
 
-devtools::check_rhub()
+rhub::rc_submit(platforms = "atlas")
 
 devtools::release()
